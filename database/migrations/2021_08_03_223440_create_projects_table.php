3d916fbf8e3f40f92d04bcdf1ffd1729e9ed408c
Pahrul Irfan <ip.5090@gmail.com>
2021-08-04 06:41:18 +0800

    update migrasi

---
+++ database/migrations/2021_08_03_223440_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->double('anggaran');
+            $table->string('lokasi');
+            $table->bigInteger('developer_id')->unsigned()->index();
+            $table->foreign('developer_id')->references('id')->on('developers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
